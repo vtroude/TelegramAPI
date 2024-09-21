43a9ba5e409c5503a09553def0cbe614755695b6
vtroude <virgiletroude@gmail.com>
2024-09-21 13:17:59 +0200

    app.run

--- signal_api.py
+++ signal_api.py
@@ -79,5 +79,5 @@ def get_all_signal():
     return jsonify(signals)
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
